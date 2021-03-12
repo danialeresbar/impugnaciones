e14e42a4e7971155f250f9ecf933dab3457bd285
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-03-12 00:16:53 -0500

    add static and media urls

--- impugnaciones/urls.py
+++ impugnaciones/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from tmp import views
 from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -29,3 +31,6 @@ urlpatterns = [
     path("", include("autenticacion.urls")),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
