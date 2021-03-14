059f58c85027346d24719ca076cf2e7f560098f8
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-03-14 18:31:16 -0500

    refactor PEP first approach

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,11 +1,7 @@
-
-from .views import login_view, register_user
-
+from autenticacion.views import index, login_view, register_user
 from django.urls import path, re_path
-from . import views
 
 urlpatterns = [
     path('login/', login_view, name="login"),
-    path('', views.index, name='home'),
+    path('', index, name='home'),
 ]
-
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,37 +1,29 @@
-from django.shortcuts import render
-
-from django.shortcuts import render, redirect
+from django import template
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
-from django.forms.utils import ErrorList
-from django.http import HttpResponse
-from .forms import LoginForm, SignUpForm
 from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.models import User
+# from django.forms.utils import ErrorList
+from django.http import HttpResponse
+# from django.template import loader
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
-from django.http import HttpResponse
-from django import template
-from django.template import loader
 
+from autenticacion.forms import LoginForm, SignUpForm
 
 
 @login_required(login_url="/login/")
 def index(request):
-    context = {}
+    context = dict()
     context['segment'] = 'index'
-
     html_template = loader.get_template('index.html')
     return HttpResponse(html_template.render(context, request))
 
 
-
 def login_view(request):
     form = LoginForm(request.POST or None)
-
     msg = None
 
     if request.method == "POST":
-
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
@@ -44,11 +36,11 @@ def login_view(request):
         else:
             msg = 'Error validating the form'    
 
-    return render(request, "accounts/login.html", {"form": form, "msg" : msg})
+    return render(request, "accounts/login.html", {"form": form, "msg": msg})
+
 
 def register_user(request):
-
-    msg     = None
+    msg = None
     success = False
 
     if request.method == "POST":
@@ -59,24 +51,23 @@ def register_user(request):
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
 
-            msg     = 'User created.'
+            msg = 'User created.'
             success = True
             
-            #return redirect("/login/")
+            # return redirect("/login/")
 
         else:
             msg = 'Form is not valid'    
     else:
         form = SignUpForm()
 
-    return render(request, "accounts/register.html", {"form": form, "msg" : msg, "success" : success })
+    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
 
 
 @login_required(login_url="/login/")
 def index(request):
-    context = {}
+    context = dict()
     context['segment'] = 'index'
-
     html_template = loader.get_template('index.html')
     return HttpResponse(html_template.render(context, request))
 
@@ -87,19 +78,15 @@ def pages(request):
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
     try:
-
         load_template = request.path.split('/')[-1]
         context['segment'] = load_template
-
         html_template = loader.get_template(load_template)
         return HttpResponse(html_template.render(context, request))
-
     except template.TemplateDoesNotExist:
-
         html_template = loader.get_template('page-404.html')
         return HttpResponse(html_template.render(context, request))
 
-    except:
-
+    except Exception as e:
+        print(f'Error: \n{e}')
         html_template = loader.get_template('page-500.html')
         return HttpResponse(html_template.render(context, request))
